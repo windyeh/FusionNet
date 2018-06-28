045669a60aac84665bde56e1b9cea9443a68fb45
Wind Yeh <wind.yeh+github@gmail.com>
2018-06-28 23:59:25 +0800

    Update data.py

--- data.py
+++ data.py
@@ -117,10 +117,10 @@ class DataEngine(Dataset):
 
 
 if __name__ == '__main__':
-    # train = load_data('train.json')
-    # test = load_data('test.json')
+    train = load_data('train.json')
+    test = load_data('test.json')
     # train = load_data('train_squad.json')
     # test = load_data('dev_squad.json')
-    train = load_data('FusionNet/squad/train-v1.1.json')
-    test = load_data('FusionNet/squad/dev-v1.1.json')
+    # train = load_data('FusionNet/squad/train-v1.1.json')
+    # test = load_data('FusionNet/squad/dev-v1.1.json')
     print(len(train), len(test))
