3cc73f3e9731c98031e27e6811a1d7cb4a8bd90b
Wind Yeh <wind.yeh+github@gmail.com>
2018-06-28 09:06:04 +0800

    Update train.py

--- train.py
+++ train.py
@@ -35,8 +35,10 @@ parser.add_argument('--embedding_source', type=str, default='./', help='pretrain
 args = parser.parse_args()
 
 if __name__ == '__main__':
-    train = data.load_data('train.json', args.word_base)
-    test = data.load_data('test.json', args.word_base)
+    # train = data.load_data('train.json', args.word_base)
+    # test = data.load_data('test.json', args.word_base)
+    train = data.load_data('train_squad.json', args.word_base)
+    test = data.load_data('dev_squad.json', args.word_base)
     vocabulary, pad_lens = data.build_vocab(train, test, args.vocab_size)
     print('Vocab size: %d | Max context: %d | Max question: %d'%(
           len(vocabulary), pad_lens[0], pad_lens[1]))
