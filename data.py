24b4a871d70019abeb851b11701a65452d561edd
Wind Yeh <wind.yeh+github@gmail.com>
2018-06-28 08:57:53 +0800

    Update data.py

--- data.py
+++ data.py
@@ -117,6 +117,8 @@ class DataEngine(Dataset):
 
 
 if __name__ == '__main__':
-    train = load_data('train.json')
-    test = load_data('test.json')
+    # train = load_data('train.json')
+    # test = load_data('test.json')
+    train = load_data('./squad/train-v1.1.json')
+    test = load_data('./squad/dev-v1.1.json')
     print(len(train), len(test))
