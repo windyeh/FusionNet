9d95ec53a18b0cbb4966cbe41b3ee8b480e2bfd2
Wind Yeh <wind.yeh+github@gmail.com>
2018-06-29 13:11:28 +0800

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -141,8 +141,8 @@ def preprocess_squad(file_name, output_name, skip_answer=False):
     json.dump(examples, open(output_name, 'w'))
 
 if __name__ == '__main__':
-    preprocess_ch('~/DRCD/DRCD_training.json', 'train.json')
-    preprocess_ch('~/DRCD/DRCD_dev.json', 'test.json', True)
+    preprocess_ch('/content/DRCD/DRCD_training.json', 'train.json')
+    preprocess_ch('/content/DRCD/DRCD_dev.json', 'test.json', True)
     # preprocess_ch('DRCD_training.json', 'train.json')
     # preprocess_ch('DRCD_dev.json', 'test.json', True)
     #preprocess_squad('FusionNet/squad/train-v1.1.json', 'train_squad.json')
